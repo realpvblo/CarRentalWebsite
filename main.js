2bb276c3c4d525e77abb71d2215689d65e03a557
realpvblo <78243008+realpvblo@users.noreply.github.com>
2022-06-21 01:51:42 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,18 +3,3 @@ $(document).ready(function(){
       $("#myModal").modal();
     });
   });
-
-  // Your web app's Firebase configuration
-var firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-// Initialize variables
-const auth = firebase.auth()
-const database = firebase.database()
